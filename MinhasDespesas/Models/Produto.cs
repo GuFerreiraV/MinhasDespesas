54da2ae84310ab14b6e6fd0157b1b3465f62d9a3
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-11 16:09:59 -0300

    Criação das pastas, seguindo uma arquitetura limpa

--- MinhasDespesas/Models/Produto.cs
+++ MinhasDespesas/Models/Produto.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
 
 namespace MinhasDespesas.Models
 {
-    internal class Produto
+    public class Produto
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int QtdeProduto { get; set; }
+        public double ValorProduto { get; set; }
     }
 }
