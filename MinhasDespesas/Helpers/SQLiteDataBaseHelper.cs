c6361fd5d7cffe0ca7e498aab339b293bc675908
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-14 14:10:12 -0300

    configurando o crud no sql (insert, delete,update,getall)

--- MinhasDespesas/Helpers/SQLiteDataBaseHelper.cs
+++ MinhasDespesas/Helpers/SQLiteDataBaseHelper.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MinhasDespesas.Models;
+using SQLite;
 
 namespace MinhasDespesas.Helpers
 {
-    internal class SQLiteDataBaseHelper
+    public class SQLiteDataBaseHelper
     {
+
+        readonly SQLiteAsyncConnection _connectionAsync;
+
+        public SQLiteDataBaseHelper(string Path) 
+        {
+            _connectionAsync = new SQLiteAsyncConnection(Path);
+            // cria a tabela Produto se não existir no db
+            _connectionAsync.CreateTableAsync<Produto>().Wait();
+
+        }
+
+        public Task<int> Insert(Produto prod) 
+        {
+            return _connectionAsync.InsertAsync(prod);
+        }
+
+        public Task<List<Produto>> Update(Produto prod)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, QtdeProduto=?, ValorProduto=? WHERE Id=?";
+            return _connectionAsync.QueryAsync<Produto>(
+                sql,
+                prod.Descricao,
+                prod.QtdeProduto,
+                prod.ValorProduto,
+                prod.Id
+                );
+        }
+
+        public Task<int> Delete(int ID)
+        {
+            return  _connectionAsync.Table<Produto>().DeleteAsync(i => i.Id == ID);
+        }
+
+        public Task<List<Produto>> GetAll() 
+        {
+            return _connectionAsync.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string query)
+        {
+            string sql = "SELECT * FROM Produto WHERE Descricao LIKE '%"+ query + "%' ";
+            return _connectionAsync.QueryAsync<Produto>(sql);
+        }
+
     }
 }
