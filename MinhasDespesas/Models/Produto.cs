455d66ad0fff152f8ceff386a5a582475ee71a74
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-14 17:07:13 -0300

    Adicionando nova propriedade Total

--- MinhasDespesas/Models/Produto.cs
+++ MinhasDespesas/Models/Produto.cs
@@ -9,5 +9,12 @@ namespace MinhasDespesas.Models
         public string Descricao { get; set; }
         public int QtdeProduto { get; set; }
         public double ValorProduto { get; set; }
+        public double Total 
+        { 
+            get 
+            {
+                return QtdeProduto * ValorProduto;
+            }
+        }
     }
 }
