f1f50e6ebeff247e48cfbddfdd28ebb7102044b5
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-14 15:25:13 -0300

    att

--- MinhasDespesas/App.xaml.cs
+++ MinhasDespesas/App.xaml.cs
@@ -1,7 +1,31 @@
-﻿namespace MinhasDespesas
+﻿using MinhasDespesas.Helpers;
+
+namespace MinhasDespesas
 {
     public partial class App : Application
     {
+
+        static SQLiteDataBaseHelper _db;
+
+        public static SQLiteDataBaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        // Environment: prove infos do ambiente de execução (Windows e Android no caso)
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData
+                            ), "banco_sqlite_compras.db3"
+                        );
+                    
+                    _db = new SQLiteDataBaseHelper(path);
+                }
+                
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
