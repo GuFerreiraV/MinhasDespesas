0c7ceb2cf7d7fba9751e366e32638579752df2fc
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-14 15:24:27 -0300

    Lógica de inserção de dados feita

--- MinhasDespesas/Views/NovoProduto.xaml.cs
+++ MinhasDespesas/Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MinhasDespesas.Models;
+
 namespace MinhasDespesas.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +8,24 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto prod = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				QtdeProduto = Convert.ToInt32(txt_quantidade.Text),
+				ValorProduto = Convert.ToDouble(txt_preco.Text)
+			};
+            // inserindo o produto no banco de dados
+            await App.Db.Insert(prod);
+			await DisplayAlert("Sucesso", "Registro sucedido", "Ok");
+		}
+		catch (Exception ex) 
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
 }
