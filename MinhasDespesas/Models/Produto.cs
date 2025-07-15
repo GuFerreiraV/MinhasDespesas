4cf073891418b85af576759d253a88083ebf3cab
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-15 11:39:50 -0300

    validação do insert

--- MinhasDespesas/Models/Produto.cs
+++ MinhasDespesas/Models/Produto.cs
@@ -4,14 +4,27 @@ namespace MinhasDespesas.Models
 {
     public class Produto
     {
+        string _descricao;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (string.IsNullOrEmpty(value)) 
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+                _descricao = value; 
+            }
+        }
         public int QtdeProduto { get; set; }
         public double ValorProduto { get; set; }
-        public double Total 
-        { 
-            get 
+        public double Total
+        {
+            get
             {
                 return QtdeProduto * ValorProduto;
             }
